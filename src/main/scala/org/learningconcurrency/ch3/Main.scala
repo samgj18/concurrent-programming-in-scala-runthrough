f7fce6f800a5fb8bb8cbadca282eff90397e8550
samgj18 <samgj_18@hotmail.com>
2023-03-25 11:53:28 +0000

    Adding Lazy Vals examples

--- src/main/scala/org/learningconcurrency/ch3/Main.scala
+++ src/main/scala/org/learningconcurrency/ch3/Main.scala
@@ -117,10 +117,9 @@ object AtomicUid extends App {
   ECUtil.execute(Instantiator.log(s"Got unique ID: ${getUniqueIdSynchronized}"))
   Instantiator.log(s"Got another unique ID: ${getUniqueIdSynchronized}")
 
- 
   // CAS (compare-and-swap), compare and swap is the fundamental building block of
   // all atomic operations. `incrementAndGet`, `addAndGet`, `getAndSet` ... are
-  // all implemented using CAS operations. Conceptually, a CAS operation is the 
+  // all implemented using CAS operations. Conceptually, a CAS operation is the
   // equivalent of the following code:
   // def compareAndSet(oldValue: Long, newValue: Long): Boolean = {
   //     this.synchronized {
@@ -132,7 +131,7 @@ object AtomicUid extends App {
   //          }
   //      }
   // }
-  
+
   // Let's reimplement the getUniqueId method using a CAS operation
   @tailrec
   def getUniqueIdCAS: Long = {
@@ -151,8 +150,11 @@ object AtomicLock extends App {
   private val lock = new AtomicBoolean(false)
 
   def mySynchronized(body: => Unit): Unit = {
-    while(!lock.compareAndSet(false, true)) {} // busy waiting as it constantly checks the value of the lock
-    try body finally lock.set(false)
+    while (
+      !lock.compareAndSet(false, true)
+    ) {} // busy waiting as it constantly checks the value of the lock
+    try body
+    finally lock.set(false)
   }
   var count = 0
   for (i <- 0 until 10) ECUtil.execute {
@@ -162,4 +164,90 @@ object AtomicLock extends App {
   }
   Thread.sleep(1000)
   Instantiator.log(s"Count is $count")
+}
+
+// FileSystem API
+
+object FileSystem {
+  // 1. Ensure that only one thread can delete the file at the same time
+
+  sealed trait State
+  object Idle extends State
+  object Creating extends State
+
+  /** @param n
+    *   how many concurrent copies are in progress
+    */
+  final case class Copying(val n: Int) extends State
+  object Deleting extends State
+
+  /** Model of a single file or directory entry in a file system.
+    *
+    * @param isDir
+    *   true if the entry is a directory, false if it is a file
+    */
+  class Entry(val isDir: Boolean) {
+    val state = new AtomicReference[State](Idle)
+  }
+
+  @tailrec private def prepareForDelete(entry: Entry): Boolean = {
+    val s0 = entry.state.get()
+
+    s0 match {
+      case Idle =>
+        // An important note about CAS instructions on atomic references is that uses reference equality and never calls equal method (even if it is overridden).
+        // Note:
+        // Referential equality is whether two references point to the same object in a heap -> Review playground.worksheet.sc
+        if (entry.state.compareAndSet(s0, Deleting)) true
+        else prepareForDelete(entry)
+      case Creating =>
+        Instantiator.log("File currently created, cannot delete"); false
+      case Copying(n) =>
+        Instantiator.log("File currently copied, cannot delete"); false
+      case Deleting => false
+    }
+  }
+
+}
+
+object LazyValsCreate extends App {
+  lazy val obj = new AnyRef
+  lazy val non = s"made by ${Thread.currentThread.getName}"
+
+  ECUtil.execute {
+    Instantiator.log(s"EC sees obj = $obj")
+    Instantiator.log(s"EC sees non = $non")
+  }
+  Instantiator.log(s"Main sees obj = $obj")
+  Instantiator.log(s"Main sees non = $non")
+  Thread.sleep(500)
+}
+
+// Running this program reveals that the Lazy initializer runs when the
+// object is first referenced in the third line and not when it is declared.
+object LazyValsObject extends App {
+  object Lazy { Instantiator.log("Running Lazy constructor.") }
+  Instantiator.log("Main thread is about to reference Lazy.")
+  Lazy
+  Instantiator.log("Main thread completed.")
+}
+
+// Singleton objects are implemented with the so-called double-checked locking idiom under the hood
+// This concurrent programming pattern ensures that a lazy value is initialized by at most one thread when it is first accessed.
+object LazyValsUnderTheHood extends App {
+  // The @volatile annotation ensures that this field is visible to all threads.
+  @volatile private var _bitmap = false
+  private var _obj: AnyRef = _
+  def obj = if (_bitmap) _obj
+  else
+    this.synchronized {
+      // Here is where the double-checked locking idiom happens, 
+      if (!_bitmap) {
+        _obj = new AnyRef
+        _bitmap = true
+      }
+      _obj
+    }
+  Instantiator.log(s"$obj")
+  Instantiator.log(s"$obj")
 }
---
+++ src/main/scala/org/learningconcurrency/ch3/playground.worksheet.sc
@@ -0,0 +1,46 @@
+// Equality
+val newString = new String("Hello")
+val anotherString = new String("Hello")
+
+// Reference equality
+newString eq anotherString // false
+
+// Value equality
+newString == anotherString // true
+
+// Equals and HashCode note:
+//
+// Unless we define a case class where hashcode() and equals() are already appropriately defined for us, we need to override them, otherwise
+// equality might not work as expected.
+
+class Person(val name: String, val age: Int)
+
+val p1 = new Person("John", 30)
+val p2 = new Person("John", 30)
+
+p1 == p2 // false <- this is because we haven't overridden equals() and hashcode() methods
+
+// We can override equals() and hashcode() methods as follows:
+class PersonOverride(val name: String, val age: Int) {
+  override def equals(other: Any): Boolean = other match {
+    case person: PersonOverride =>
+      this.name == person.name && this.age == person.age
+    case _ => false
+  }
+
+  override def hashCode(): Int =
+    if (name eq null) age else name.hashCode + 31 * age
+}
+
+// Or simply:
+case class PersonCaseClass(val name: String, val age: Int)
+
+val p1c = new PersonCaseClass("John", 30)
+val p2c = new PersonCaseClass("John", 30)
+
+p1c == p2c // true
+
+val p3o = new PersonOverride("John", 30)
+val p4o = new PersonOverride("John", 30)
+
+p3o == p4o // true
